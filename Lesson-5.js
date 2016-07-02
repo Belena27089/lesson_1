ac36e8e9a15307b692fa125a881b495f412df471
Belena27089 <bistrova71@yandex.ru>
2016-07-03 02:33:23 +0600

    убрала ввод из функций

--- Lesson-5.js
+++ Lesson-5.js
@@ -1,36 +1,37 @@
  // Функция преобразования цвета из 10-ного представления в 16-ный
- var colorToHex = function () {  
-    var nums = [];
-    for (var j = 0; j < 3; j++){
-       var num = prompt('Число от 0 до 256');
+  
+    // var nums = [];
+    // for (var j = 0; j < 3; j++){
 
-          while (num < 0 || num > 255){
-                num = prompt('Число от 0 до 256');
-          }
-           nums.push(num);
-    }   
-  var numtoString = nums.map(function(numer) {   
+    //     var num = prompt('Число от 0 до 256');
+
+    //       while ( num < 0 || num > 255){
+    //             num = prompt('Число от 0 до 256');
+    //       }
+    //        nums.push(num);
+    // } 
+  var colorToHex = function () {   
+    var numtoString = nums.map(function(num) {   
     
-    return (numer < 10 && numer >= 0) ? (0 + parseInt(numer).toString(16)) : parseInt(numer).toString(16);
+      return (num < 10 && num >= 0) ? (0 + parseInt(num).toString(16)) : parseInt(num).toString(16);
     
- });
-  return numtoString = console.log('#' + numtoString.join(''));
+  });
+  return numtoString = '#' + numtoString.join('');
 } 
 
 console.log(colorToHex());
 
  
 
-// Функция, преобразующую число в объект
+// Функция, преобразующую число в объект 
+
   
- 
+     //  var char = prompt();
+     //  if (char > 999){
+     //   char = prompt('Вы ввели слишком большое число,надо не больше 999');
+     // }
+     var  convertNum = function (){
 
-  var  convertNum = function (){
-
-      var char = prompt();
-      if (char > 999){
-       char = prompt('Вы ввели слишком большое число,надо не больше 999');
-     }
       var newchar = char.split('').reverse();    
 
       var num = {};
@@ -38,7 +39,8 @@ console.log(colorToHex());
       num['десятки'] = newchar[1]; 
       num['сотни'] = newchar[2];
       return num;
-  }  
+  }
+    
   console.log(convertNum()); 
 
 
@@ -46,17 +48,15 @@ console.log(colorToHex());
  
 user = {firstName: 'Dmitry', lastName: 'Bondarchuk', birthDay: '16.07.1990', phones: ['89001234567', '83431234567']};
  
+ 
 function toQueryString(user) {
     var str = "";
   
-    for (var i in user){
-        if (!true) {
-         str += "&";
-       }
-        str += i + "=" + user[i];
-        
+    for (var i in user){  
+
+      if(str.length != 0)
+        str += "&";
+        str += i + "="+user[i];        
     }
     return str;
 }
-str = toQueryString({});
-console.log(str);
